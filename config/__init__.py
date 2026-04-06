749fc25115bdff974523eb6daf129d1b778eb0f9
Mohammad Aaban <mohammadaaban4@gmail.com>
2026-04-07 01:47:02 +0530

    feat: initialize project with config management

---
+++ config/__init__.py
@@ -0,0 +1,33 @@
+import os
+import yaml
+from dotenv import load_dotenv
+
+class AppConfig:
+    def __init__(self):
+        load_dotenv()
+        self.config = self.load_config()
+
+    def load_config(self):
+
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        config_path = os.path.join(base_dir,'..','config.yaml')
+
+        with open(config_path,'r') as f:
+            config = yaml.safe_load(f)
+
+        if not config:
+            raise ValueError("Configuration file is empty or not found.")
+        
+        if 'api' in config and 'key' in config['api']:
+            api_key = os.get_env('API_KEY')
+
+            if not api_key:
+                raise ValueError("API key not found in environment variables.")
+        
+            config['api']['key'] = api_key
+        
+        return config
+    
+
+config = AppConfig().config
+
